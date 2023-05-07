3de087bac4fb5f73ee9f6a89c54cbf97f02bd976
Cipriano Mereu <ciprianomereu@gmail.com>
2023-05-07 23:50:06 +0200

    add:style input

--- password-generator/src/Components/PasswordGenerator/PasswGen.js
+++ password-generator/src/Components/PasswordGenerator/PasswGen.js
@@ -1,30 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../../UI/Button/Button";
 import Checkbox from "../../UI/Checkbox/Checkbox";
 import "./passwGen.css";
 
 function PasswGen() {
   const [lunPassw, setLunPassw] = useState(0);
+  const [styleColor, setStyleColor] = useState({});
+  const [passwSettings, setPasswSettings] = useState({
+    numbers: true,
+    lowercase: true,
+    uppercase: true,
+    symbols: true,
+  });
+
+  const [password, setPassword] = useState("password");
+
+  useEffect(() => {
+    if (lunPassw >= 4 && lunPassw < 8) {
+      setStyleColor({ backgroundColor: "#df6661", width: "25%" });
+    } else if (lunPassw >= 8 && lunPassw < 12) {
+      setStyleColor({ backgroundColor: "#ffc93c", width: "50%" });
+    } else if (lunPassw >= 12 && lunPassw < 16) {
+      setStyleColor({ backgroundColor: "#82c91e", width: "75%" });
+    } else if (lunPassw >= 16) {
+      setStyleColor({ backgroundColor: "#059c02", width: "100%" });
+    } else {
+      setStyleColor({ backgroundColor: "none", width: "0%" });
+    }
+  }, [lunPassw]);
 
   return (
     <div className="pass-container">
       <div className="pass-content-cont">
         <h1>Password Generator</h1>
-        <div>Form</div>
+        <div>
+          <div className="passw-generator">
+            <input type="text" value={password} />
+          </div>
+          <div className="passw-generator-color" style={styleColor}></div>
+        </div>
         <h3>Password Length:</h3>
         <div className="pass-range">
-          <input type="number" min="0" max="50" value={lunPassw} />
+          <input
+            type="number"
+            min="0"
+            max="50"
+            value={lunPassw}
+            onChange={(ev) => setLunPassw(ev.target.value)}
+          />
           <input
             type="range"
             min="0"
             max="50"
+            value={lunPassw}
             onChange={(ev) => setLunPassw(ev.target.value)}
           />
         </div>
         <div>
           <h3>Password Settings:</h3>
 
-          <Checkbox />
+          <Checkbox
+            passwSettings={passwSettings}
+            setPasswSettings={setPasswSettings}
+          />
         </div>
         <Button>Generate Password</Button>
       </div>
--- password-generator/src/UI/Checkbox/Checkbox.js
+++ password-generator/src/UI/Checkbox/Checkbox.js
@@ -1,23 +1,55 @@
 import "./checkbox.css";
 
-function Checkbox() {
+function Checkbox({ passwSettings, setPasswSettings }) {
   return (
     <div className="container-checkbox">
-      <label for="checkbox-1">
-        <input type="checkbox" id="checkbox-1" name="checkbox-1" />
+      <label htmlFor="checkbox-1">
+        <input
+          type="checkbox"
+          id="checkbox-1"
+          name="checkbox-1"
+          checked={passwSettings.numbers}
+          onChange={(ev) =>
+            setPasswSettings({ ...passwSettings, numbers: ev.target.checked })
+          }
+        />
         Numbers
       </label>
-      <label for="checkbox-2">
-        <input type="checkbox" id="checkbox-2" name="checkbox-2" />
+      <label htmlFor="checkbox-2">
+        <input
+          type="checkbox"
+          id="checkbox-2"
+          name="checkbox-2"
+          checked={passwSettings.lowercase}
+          onChange={(ev) =>
+            setPasswSettings({ ...passwSettings, lowercase: ev.target.checked })
+          }
+        />
         Lowercase
       </label>
 
-      <label for="checkbox-3">
-        <input type="checkbox" id="checkbox-3" name="checkbox-3" />
+      <label htmlFor="checkbox-3">
+        <input
+          type="checkbox"
+          id="checkbox-3"
+          name="checkbox-3"
+          checked={passwSettings.uppercase}
+          onChange={(ev) =>
+            setPasswSettings({ ...passwSettings, uppercase: ev.target.checked })
+          }
+        />
         Uppercase
       </label>
-      <label for="checkbox-4">
-        <input type="checkbox" id="checkbox-4" name="checkbox-4" />
+      <label htmlFor="checkbox-4">
+        <input
+          type="checkbox"
+          id="checkbox-4"
+          name="checkbox-4"
+          checked={passwSettings.symbols}
+          onChange={(ev) =>
+            setPasswSettings({ ...passwSettings, symbols: ev.target.checked })
+          }
+        />
         Symbols
       </label>
     </div>
