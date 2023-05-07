08fb2d888277a6cbf41146a5dc87abbc7f5f8280
Cipriano Mereu <ciprianomereu@gmail.com>
2023-05-07 20:27:35 +0200

    add:range style

--- password-generator/src/Components/PasswordGenerator/PasswGen.js
+++ password-generator/src/Components/PasswordGenerator/PasswGen.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import Button from "../../UI/Button/Button";
 import Checkbox from "../../UI/Checkbox/Checkbox";
 import "./passwGen.css";
 
 function PasswGen() {
+  const [lunPassw, setLunPassw] = useState(0);
+
   return (
     <div className="pass-container">
       <div className="pass-content-cont">
         <h1>Password Generator</h1>
         <div>Form</div>
-        <div>
-          <h3>Password Length:</h3>
-          <input type="number" min="0" max="50" />
-          <input type="range" min="0" max="50" />
+        <h3>Password Length:</h3>
+        <div className="pass-range">
+          <input type="number" min="0" max="50" value={lunPassw} />
+          <input
+            type="range"
+            min="0"
+            max="50"
+            onChange={(ev) => setLunPassw(ev.target.value)}
+          />
         </div>
         <div>
           <h3>Password Settings:</h3>
